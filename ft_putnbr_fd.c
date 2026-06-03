a568c43eb92a499c488cd272bebd5d4f1d209200
Nicholas Smith <nsmith@c4a2c9.42berlin.de>
2026-06-03 21:05:20 +0200

    Adding files.

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsmith <nsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 20:48:30 by nsmith            #+#    #+#             */
+/*   Updated: 2026/06/03 20:48:51 by nsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int num, int fd)
+{
+	long	number;
+
+	number = num;
+	if (number < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number = -number;
+	}
+	if (number >= 10)
+	{
+		ft_putnbr_fd(number / 10, fd);
+		ft_putchar_fd((number % 10) + '0', fd);
+	}
+	else
+		ft_putchar_fd(nbr + '0', fd);
+}
