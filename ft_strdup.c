f4711286b50922e687a288aab3868780c358e32c
Nicholas Smith <nsmith@c4a14c10.42berlin.de>
2026-06-04 17:01:48 +0200

    Adding files.

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsmith <nsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 16:46:28 by nsmith            #+#    #+#             */
+/*   Updated: 2026/06/04 16:57:33 by nsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*dup;
+	int		i;
+
+	dup = (char *)malloc(strlen(*str) + 1);
+	if (!dup)
+		return (NULL);
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = 0;
+	return (dup);
+}
---
+++ ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsmith <nsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 16:31:54 by nsmith            #+#    #+#             */
+/*   Updated: 2026/06/04 16:58:53 by nsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *strrchr(const char *str, int chr)
+{
+	int end;
+
+	end = ft_strlen(*str);
+	while (end >= 0)
+	{
+		if (str[end] == chr)
+			return((char *)str);
+		end--;
+	}
+	return ((char *)str);
+}
